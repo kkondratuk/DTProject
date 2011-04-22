85f2a5dcce0b81eaf25f88a6b5ecbf4639baff1f
Constantine Kondratyuk <k.kondratuk@list.ru>
2011-04-22 14:03:31 +0300

    Create project.

---
+++ TwistService.asmx.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Services;
+
+namespace TestWebService
+{
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+
+    public class TwistService : WebService
+    {
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
